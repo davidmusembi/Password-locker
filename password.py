7922a4fa3945d4dde78bca364ea53f1d2e7544a2
davidmusembi <davidmusembi02@gmail.com>
2022-04-24 23:09:08 +0300

    method that generates a random password

--- password.py
+++ password.py
@@ -71,3 +71,10 @@ class Credentials():
     @classmethod
     def display_credentials(cls):
         return cls.credentials_list
+    
+    def generatePassword(stringLength=8):
+        """
+        Generate a random password string of letters and digits and special characters
+        """
+        password = string.ascii_uppercase + string.ascii_lowercase + string.digits + "~!@#$%^&*"
+        return ''.join(random.choice(password) for i in range(stringLength))
