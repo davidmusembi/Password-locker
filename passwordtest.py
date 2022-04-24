7bedf04dc4bf8dfd5d15a60848fa66e242df64a4
davidmusembi <davidmusembi02@gmail.com>
2022-04-24 23:25:38 +0300

    Class to test the credentials

--- passwordtest.py
+++ passwordtest.py
@@ -13,14 +13,31 @@ class TestClass(unittest.TestCase):
         
     def test_init(self):
         """
-        method to initialize the test class with the given credentials and test if the user is created successfully
+        test case to initialize the test class with the given credentials and test if the user is created successfully
         """
         self.assertEqual(self.new_user.username,'Dave')
         self.assertEqual(self.new_user.password,'Qfg4hbg9')
         
         def test_save_user(self):
             """
-            method to save the user to the test class
+            test case to save the user to the test class
             """
             self.new_user.save_user()
             self.assertEqual(len(User.user_list),1)
+            
+        class TestCredentials(unittest.TestCase):
+            """
+            test case to test credentials for the test class
+            """
+            def setUp(self):
+                """
+                method to set up the test class with the given credentials
+                """
+                self.new_credential = Credentials('Instagram','Dave','Wg8htb52')
+    def test_init(self):
+        """
+        test case to initialize the test class with the given credentials and  test credentials
+        """
+        self.assertEqual(self.new_credential.account,'Instagram')
+        self.assertEqual(self.new_credential.userName,'Dave')
+        self.assertEqual(self.new_credential.password,'Wg8htb52')
