fb03f8440ae2d95eb432d91e8667cc5ffacd421d
davidmusembi <davidmusembi02@gmail.com>
2022-04-24 22:57:30 +0300

    Create a class user

---
+++ password.py
@@ -0,0 +1,13 @@
+import random
+import string
+import pyperclip
+
+class User:
+    """
+    class User
+    """
+    user_list = []
+    
+     def __init__(self, username, password):
+          self.username = username
+        self.password = password
